7d2868d35b0a6593cf89062f9c3b246f8592fb38
acarrou <dridriswim@gmail.com>
2022-05-05 11:39:43 -0700

    deleted files ad cleanedup

--- flask_project_T5/project/forms.py
+++ flask_project_T5/project/forms.py
@@ -72,7 +72,7 @@ class RequestResetForm(FlaskForm):
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is None:
-            raise ValidationError('There is no account with that email. You must register first.')
+            raise ValidationError('There is no account with that email. You must signup first.')
 
 
 class ResetPasswordForm(FlaskForm):
