40169754977893c4b0102767589b84ea6a4279bc
Adrien Carrou <dridriswim@gmail.com>
2022-05-05 00:06:40 -0700

    full account login and create with selling page almost completed

--- flask_project_T5/project/__init__.py
+++ flask_project_T5/project/__init__.py
@@ -1,19 +1,23 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
-myapp_obj = Flask(__name__)
-
-"""
-myapp_obj.config.from_mapping(
-    SECRET_KEY = 'you-will-never-guess',
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-)
-db = SQLAlchemy(myapp_obj)
-login = LoginManager(myapp_obj)
-login.login_view = 'login'
-"""
 from project import routes
--- flask_project_T5/run.py
+++ flask_project_T5/run.py
@@ -1,2 +1,4 @@
-from project import myapp_obj
-myapp_obj.run(debug=True)
+from project import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
