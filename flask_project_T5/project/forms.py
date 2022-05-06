5522be197f97b0cb939a1143e4de9952bf8a91dd
Faith Deanon <afdeanon@gmail.com>
2022-05-06 04:31:21 -0700

    Worked on CSS and layout of home screen and posts screen

--- flask_project_T5/project/forms.py
+++ flask_project_T5/project/forms.py
@@ -62,21 +62,3 @@ class PostForm(FlaskForm):
     item_price = StringField('Item Price $', validators=[DataRequired()])
     item_picture = FileField('Item Image', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Post')
-
-
-class RequestResetForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is None:
-            raise ValidationError('There is no account with that email. You must signup first.')
-
-
-class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Reset Password')
--- flask_project_T5/project/routes.py
+++ flask_project_T5/project/routes.py
@@ -13,7 +13,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=7)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
@@ -105,6 +105,21 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
+
+
+@app.route("/account/<int:user_id>/delete", methods=['GET','POST'])
+def delete_account(user_id):
+        try:
+            logout_user()
+            account = User.query.get_or_404(user_id)
+            posts = Post.query.get_or_404(user_id)
+            db.session.delete(account)
+            db.session.delete(posts)
+            db.session.commit()
+        except:
+            abort(404)
+        flash('Your account has been deleted!', 'success')
+        return redirect(url_for('home'))
 
 
 @app.route("/post/new", methods=['GET', 'POST'])
